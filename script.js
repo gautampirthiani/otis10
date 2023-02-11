1717116076c3d3aea42e9b0f72517d2a9a85785a
gautampirthiani <118488072+gautampirthiani@users.noreply.github.com>
2023-02-11 16:39:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const inputbox  = document.getElementById("file-input");
+const inputbox = document.getElementById("file-input");
 const table = document.getElementById("excel-table");
 
 const xlsxdata = [];
@@ -24,6 +24,31 @@ inputbox.addEventListener('change', (event) => {
             table.appendChild(tr);
             xlsxdata.push(innerlist);
         });
+
+        // Fill the API input boxes with the data from xlsxdata
+        fillAPIInputs();
     });
     console.log(xlsxdata);
 });
+
+function fillAPIInputs() {
+  const inputBoxes = {
+    unit_number: document.getElementById("unit-number-input"),
+    country_code: document.getElementById("country-code-input"),
+    customer_id: document.getElementById("customer-id-input"),
+    contract_no: document.getElementById("contract-no-input"),
+    start_date: document.getElementById("start-date-input"),
+    end_date: document.getElementById("end-date-input"),
+  };
+
+  // Assuming the data is stored in the first row of the xlsxdata array
+  const data = xlsxdata[0];
+
+  // Fill the input boxes with the data
+  inputBoxes.unit_number.value = data[0];
+  inputBoxes.country_code.value = data[1];
+  inputBoxes.customer_id.value = data[2];
+  inputBoxes.contract_no.value = data[3];
+  inputBoxes.start_date.value = data[4];
+  inputBoxes.end_date.value = data[5];
+}
